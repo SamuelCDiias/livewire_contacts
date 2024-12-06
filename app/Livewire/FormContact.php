e14e51f3180dabb4cd2dd6706bc145d7f32256cb
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-12-06 11:37:30 -0300

    005

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+
+    public $name, $phone, $email;
+
+    public function newContact()
+    {
+        // validation
+        $this->validate([
+            'name' => 'required | min:3 | max:50',
+            'email' => 'required | email | min:5 | max:50',
+            'phone' => 'required | min:5 | max:20'
+        ]);
+
+        // temporary store in log file
+        Log::info('Novo Contato: ' . $this->name . ' - ' . $this->email . ' - ' . $this->phone);
+    }
+
     public function render()
     {
         return view('livewire.form-contact');
--- resources/views/livewire/main-component.blade.php
+++ resources/views/livewire/main-component.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="text-center my-5">
-        <img src="{{asset('assets/images/logo.png')}}" alt="logo" width="128px">
+        <img src="{{ asset('assets/images/logo.png') }}" alt="logo" width="128px">
     </div>
 
     <div class="container">
